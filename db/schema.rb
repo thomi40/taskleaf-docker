bde1f8df13048ba58df84fe7acd8e931061d9213
Tomoki Ishimaru <thom.ishimaru@gmail.com>
2020-10-04 12:54:12 +0900

    4-5-4 Add user manage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_012812) do
+ActiveRecord::Schema.define(version: 2020_10_04_020225) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_09_27_012812) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
